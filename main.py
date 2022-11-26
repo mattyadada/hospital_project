031aa985b0a08489fe8eb481ef40c50bf12fc233
mattyadada <116231531+mattyadada@users.noreply.github.com>
2022-11-26 17:29:59 -0500

    wip

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # from colorama import init
 # from termcolor import colored
 # import emoji
+import random
 import time
 from tqdm import tqdm
 from art import *
@@ -11,16 +12,15 @@ art_1 = text2art("Welcome", font="rnd-medium")
 print(art_1)
 
 # Documentation
-docs = open("READ.md", "r")
-# parell arrays
+docs = open("READ.md", "rt")
+# parallel arrays
 services = ['Xrays', 'CT Scan', 'Surgery', 'Vascular']
-prices = [350, 403, 323, 384]
-# parell arrays
+prices = [350.0, 403.2, 323.3, 384.4]
+# parallel arrays
 services_used = []
 prices_used = []
 
 medicine = []
-multi = ['a', 'b', 'c', 'd']
 
 
 def custom_services(custom_selection):
@@ -31,12 +31,13 @@ def custom_services(custom_selection):
         while custom_selection != 'q':
             custom_service = str(input("Enter the additional service used: "))
             if custom_service == 'q':
-                services_used.append(custom_service)
                 break
             custom_price = int(input("Enter the price of the service: $"))
             if custom_price == 'q':
                 prices_used.append(custom_price)
                 break
+            prices_used.append(custom_price)
+            services_used.append(custom_service)
 
     elif custom_selection == 'n':
         pass
@@ -44,8 +45,6 @@ def custom_services(custom_selection):
         print("Not a valid input")
         custom_services(custom_selection)
     print("Thanks for your input! Here are all the services used")
-    # services_used.append(custom_service)
-    #  prices_used.append(custom_price)
 
     print(f"--- services used---")
     for (i, j) in zip(services_used, prices_used):
@@ -53,16 +52,17 @@ def custom_services(custom_selection):
 
 
 def room_rate():
-    input("Enter how many days in the hospital you spent ")
+    user_room_rate = input("Enter how many days in the hospital you spent ")
+    return user_room_rate
 
 
 def medicine_choice():
     print("something")
 
 
-def service_choice():
-    user_choice_multi = " "
+def service_choice(insurance_discount, pay_choice):
     final_choice = 0
+    multi = ['a', 'b', 'c', 'd']
     print("\n")
     print("************ Enter the service that used ************\n")
 
@@ -70,31 +70,54 @@ def service_choice():
         print("\t\t\t----------------------\n")
         print(f"\t\t\t      {j} {i}      \n")
 
-    while user_choice_multi.lower() != 'q':
-        user_choice_multi = str(input("Enter Service Used (type 'q' when finished): "))
-        user_choice_multi = user_choice_multi.lower()
-        if user_choice_multi.lower() == multi[0]:
+    while pay_choice == 'y':
+        user_choice_services = str(input("Enter Service Used (type 'q' when finished): "))
+        user_choice_services = user_choice_services.lower()
+        if user_choice_services.lower() == multi[0]:
             services_used.append(services[0])
             prices_used.append(prices[0])
             final_choice += prices[0]
-        elif user_choice_multi.lower() == multi[1]:
+        elif user_choice_services.lower() == multi[1]:
             services_used.append(services[1])
             prices_used.append(prices[1])
             final_choice += prices[1]
-        elif user_choice_multi.lower() == multi[2]:
+        elif user_choice_services.lower() == multi[2]:
             services_used.append(services[2])
             prices_used.append(prices[2])
             final_choice += prices[2]
-        elif user_choice_multi.lower() == multi[3]:
+        elif user_choice_services.lower() == multi[3]:
             services_used.append(services[3])
             prices_used.append(prices[3])
             final_choice += prices[3]
-        elif user_choice_multi.lower() == 'q':  # WIP update so user will not crash program with invalid value -matt
+        elif user_choice_services.lower() == 'q':  # WIP update so user will not crash program with invalid value -matt
+            print("Thank you for your selection")
+            break
+    print(final_choice)
+
+    while pay_choice.lower() == 'n':
+        user_choice_services = str(input("Enter Service Used (type 'q' when finished): "))
+        user_choice_services = user_choice_services.lower()
+        if user_choice_services.lower() == multi[0]:
+            services_used.append(services[0])
+            prices_used.append(prices[0])
+            final_choice += prices[0] * insurance_discount
+        elif user_choice_services.lower() == multi[1]:
+            services_used.append(services[1])
+            prices_used.append(prices[1])
+            final_choice += prices[1] * insurance_discount
+        elif user_choice_services.lower() == multi[2]:
+            services_used.append(services[2])
+            prices_used.append(prices[2])
+            final_choice += prices[2] * insurance_discount
+        elif user_choice_services.lower() == multi[3]:
+            services_used.append(services[3])
+            prices_used.append(prices[3])
+            final_choice += prices[3] * insurance_discount
+        elif user_choice_services.lower() == 'q':  # WIP update so user will not crash program with invalid value -matt
             print("Thank you for your selection")
             break
         print(final_choice)
 
-    print(final_choice)
 
     # outputs correctly make it easier to read WIP
     # sum = final_choice * 450
@@ -102,36 +125,41 @@ def service_choice():
 
 
 def final_statement(today):
-    print("Calculating bill... ")
+    nurse_first_name = ["bob", "sally", "joel", "henry", "lucy"]
+    nurse_last_name = ["smith", "rodriguez", "jefferson", "chin"]
+    random_FN = random.choice(nurse_first_name)
+    random_LN = random.choice(nurse_last_name)
+    print("\nCalculating bill... ")
     for _ in tqdm(range(100)):
         time.sleep(.04)
     print("\n")
-    print("++++++++This is your final bill++++++++")
-    print(f"{today: %B %d, %Y}")
+    print("\t\t\t\t++++++++This is your final bill++++++++")
+    print(f"{today: %B %d, %Y}\t\t\t\t\tNurse: {random_FN}, {random_LN}")
 
 
 def inpatient():
-    # Date for final bill
     today = datetime.datetime.today()
     while True:
         pay_choice = str(input("Are you self payed? Enter y or n: "))
         if pay_choice.lower() == 'y':
-            service_choice()
+            service_choice(0.25, 'y')
             # Additional Service should account for null answers
             custom_services(custom_selection=str(input("Did you have any additional services? (type y or n) ")))
             room_rate()
             medicine_choice()
             final_statement(today)
             break
-        elif pay_choice == 'n':
-            service_choice()
+        elif pay_choice.lower() == 'n':
+            print("This means that your insurance will cover most you will pay 25% of final cost")
+            service_choice(0.25, 'n')
+            custom_services(custom_selection=str(input("Did you have any additional services? (type y or n) ")))
+            room_rate()
+            medicine_choice()
+            final_statement(today)
             break
         else:
             print("Not a valid input")
             pass
-
-    # days = int(input("Number of days in the hospital: "))
-    print("This is your room rate $ ")
 
 
 def outpatient():
@@ -139,21 +167,27 @@ def outpatient():
 
 
 # This is the where the user inputs a choice
+def greeting(user_1, user_2, time_now):
+    print("Welcome to a hospital project for _____ children hospital\nEnter 'd' to refer to documentation before going "
+          "further! Enjoy\n")
+    print(f"Written by {user_1} and {user_2} {time_now}\n\n")
+
+
 def main():
-    time_now = datetime.date(2022, 11, 23)
+    greeting("Matt", "Kevin", datetime.date(2022, 11, 23))
     userChoice = input("Enter i for Inpatient o for Outpatient: ")
     userChoice = userChoice.lower()
     if userChoice == "i":
         inpatient()
     elif userChoice == "o":
         outpatient()
+    elif userChoice == "d":
+        print(docs.read())
+        main()
     else:
         print("Not a correct choice\rPlease refer to documentation ")
-        docs.read()
         # print(emoji.emojize(":grinning_face_with_big_eyes:"))
         main()
-
-    print(f"Written by Matt and Kevin {time_now}")
 
 
 if __name__ == "__main__":
